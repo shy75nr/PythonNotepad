3fa5cba02465ac35e77db293e9722e7991335b0c
shy75nr <140926566+shy75nr@users.noreply.github.com>
2023-08-05 14:01:19 +0800

    Add files via upload

---
+++ need/ttkbootstrap/themes/user.py
@@ -0,0 +1,44 @@
+USER_THEMES = {
+    "forest": {
+        "type": "light",
+        "colors": {
+            "primary": "#4582ec",
+            "secondary": "#adb5bd",
+            "success": "#02b875",
+            "info": "#17a2b8",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#F8F9FA",
+            "dark": "#343A40",
+            "bg": "#ffffff",
+            "fg": "#343a40",
+            "selectbg": "#adb5bd",
+            "selectfg": "#ffffff",
+            "border": "#bfbfbf",
+            "inputfg": "#343a40",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+    "sandstone2": {
+        "type": "light",
+        "colors": {
+            "primary": "#325D88",
+            "secondary": "#d5d5d5",
+            "success": "#93c54b",
+            "info": "#29abe0",
+            "warning": "#f47c3c",
+            "danger": "#d9534f",
+            "light": "#F8F5F0",
+            "dark": "#3E3F3A",
+            "bg": "#ffffff",
+            "fg": "#3e3f3a",
+            "selectbg": "#8e8c84",
+            "selectfg": "#ffffff",
+            "border": "#ced4da",
+            "inputfg": "#6E6D69",
+            "inputbg": "#fff",
+            "active": "#e5e5e5",
+        },
+    }
+}
